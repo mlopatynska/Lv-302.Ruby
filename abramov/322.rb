e7f1453bc336440d86c5aec1dcd4bfa55c40f540
Petro Smachylo <retrosmachylo@gmail.com>
2018-03-06 23:54:07 +0200

    Update 322.rb

--- abramov/322.rb
+++ abramov/322.rb
@@ -3,5 +3,5 @@ def sum_dividers(n)
   (1..n).inject(0) { |sum, x| sum += x if (n % x).zero?; sum }
 end
 
-sum_hash = (1..100).inject({}) { |sum, x| sum[x] = sum_dividers(x); sum }
+sum_hash = (1..10_000).inject({}) { |sum, x| sum[x] = sum_dividers(x); sum }
 puts sum_hash.key(sum_hash.values.max)
