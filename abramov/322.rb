52291c3f47b1108825e6104980666b181220e87a
Petro Smachylo <retrosmachylo@gmail.com>
2018-03-07 20:53:20 +0200

    Delete 322.rb

--- abramov/322.rb
+++
@@ -1,7 +0,0 @@
-
-def sum_dividers(n)
-  (1..n).inject(0) { |sum, x| sum += x if (n % x).zero?; sum }
-end
-
-sum_hash = (1..10_000).inject({}) { |sum, x| sum[x] = sum_dividers(x); sum }
-puts sum_hash.key(sum_hash.values.max)
