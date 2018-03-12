0f2019d22a256a6b4849db0ebaf2b08327bfd2df
anastasiabalaban <anastasiabalaban07@gmail.com>
2018-03-13 00:37:27 +0200

    added abramov tasks

--- abramov.rb
+++ abramov.rb
@@ -1,22 +1,25 @@
-def task_89a(m, n)
-  until n.zero?
-    r = m % n
-    m = n
-    n = r
+# Abramov tasks
+module Abramov
+  def task_89a(m, n)
+    until n.zero?
+      r = m % n
+      m = n
+      n = r
+    end
+    puts "The greatest common divisor is #{m}"
   end
-  puts "The greatest common divisor is #{m}"
-end
 
-def task_108(n)
-  m = 2
-  m *= 2 while m <= n
-  puts m
-end
-
-def task_331b(n)
-  arr = []
-  (1..n).to_a.repeated_combination(3).to_a.each do |el|
-    arr << "#{el}" if el[0]**2 + el[1]**2 + el[2]**2 == n
+  def task_108(n)
+    m = 2
+    m *= 2 while m <= n
+    puts m
   end
-  puts arr.count.zero? ? "It's impossible." : arr
+
+  def task_331b(n) # rubocop:disable Metrics/AbcSize
+    arr = []
+    (1..n).to_a.repeated_combination(3).each do |el|
+      arr << el.to_s if el[0]**2 + el[1]**2 + el[2]**2 == n
+    end
+    puts arr.count.zero? ? "It's impossible." : arr
+  end
 end
