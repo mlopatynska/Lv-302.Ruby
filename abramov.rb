b696b2167a2f21ed0a8a1ce66b31bea19a6b27f1
Petro Smachylo <retrosmachylo@gmail.com>
2018-03-13 13:20:27 +0200

    Update abramov.rb

--- abramov.rb
+++ abramov.rb
@@ -1,21 +1,23 @@
+# hello
 module Abramov
-def task_88в(n)
-  string_n = n.to_s
-  string_n[0], string_n[-1] = string_n[-1], string_n[0]
-  string_n.to_i
-end
-
-def square_sum(n)
-  (n.to_s.split(//).inject(0) { |sum, x| sum += x.to_i })**2
-end
-def task_329(n, m)
-  (1...n).inject([]) do |arr, x|
-  	square_sum(x) == m ? arr << x : arr
+  def task_88v(n)
+    string_n = n.to_s
+    string_n[0], string_n[-1] = string_n[-1], string_n[0]
+    string_n.to_i
   end
-end
 
-def task_562(n)
-  n_length = n.to_s.length
-  n == n.to_s.split(//).inject(0) { |sum, x| sum += (x.to_i)**n_length }
-end
+  def square_sum(n)
+    (n.to_s.split(//).inject(0) { |sum, x| sum + x.to_i })**2
+  end
+
+  def task_329(n, m)
+    (1...n).inject([]) do |arr, x|
+      square_sum(x) == m ? arr << x : arr
+    end
+  end
+
+  def task_562(n)
+    n_length = n.to_s.length
+    n == n.to_s.split(//).inject(0) { |sum, x| sum + x.to_i**n_length }
+  end
 end
