4bd3315e73f14a1fa52bb4c513b885e918521a7c
Sasha <alexmason@ukr.net>
2018-03-13 10:16:53 +0200

    abramov module 87, 181a, 562

--- abramov.rb
+++ abramov.rb
@@ -1,3 +1,25 @@
+# Abramov tasks solved
 module Abramov
-	
+  def abramov_87(n_num, m_num)
+    array = n_num.to_s.split('')
+    array.slice(- m_num, m_num).inject(0) { |sum, digit| sum + digit.to_i }
+  end
+
+  def abramov_181a(start)
+    (start..start + 49).select { |num| (num % 5).zero? }.inject(:+)
+  end
+
+  def abramov_181a_2(start)
+    range = (start..start + 49)
+    range.inject(0) { |sum, num| (num % 5).zero? ? sum + num : sum }
+  end
+
+  def abramov_562
+    (10..9999).find_all { |num| armstrong?(num) }
+  end
+
+  def armstrong?(num)
+    power = num.to_s.length
+    num.to_s.split('').inject(0) { |sum, digit| sum + digit.to_i**power } == num
+  end
 end
