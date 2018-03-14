33cc1598282d66f06b52f668b9302e926235a335
imysheniuk <i.mysheniuk@gmail.com>
2018-03-14 15:42:33 +0200

    spaces added

--- abramov_minitest.rb
+++ abramov_minitest.rb
@@ -3,15 +3,15 @@ require 'abramov'
 include Abramov
 
 class TestAbramov < Minitest::Test
-	def test_task_88a
-		assert_equal(true, task_88a(6))
-	end
+  def test_task_88a
+    assert_equal(true, task_88a(6))
+  end
 
-	def test_task_322
-		assert_equal(960, task_322(1000))
-	end
+  def test_task_322
+    assert_equal(960, task_322(1000))
+  end
 
-	def test_task_330
-		assert_includes([[6,28]], task_330(100))
-	end
+  def test_task_330
+    assert_includes([[6,28]], task_330(100))
+  end
 end
