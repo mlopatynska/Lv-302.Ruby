8f8cc2a904ea017e0b053634730b0da775e94b66
Maryan <maryan.pavliv@gmail.com>
2018-03-14 11:31:26 +0200

    added minitests

---
+++ test-abramov/test/abramov_minitest.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'abramov'
+include Abramov
+
+class TestAbramov < Minitest::Test
+	def test_task_178_b
+		assert_equal(3, task_178_b(1, 10))
+	end
+
+  def test_task_178_v
+    assert_equal(3, task_178_v(1, 36))
+  end
+
+  def test_task_554
+    assert_includes(task_554(16), [3, 4, 5])
+  end
+end
