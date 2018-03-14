f613be47f751fbb5cc1d9ca69a5ae3291473f930
Maryan <maryan.pavliv@gmail.com>
2018-03-14 16:25:01 +0200

    edited files

--- abramov_minitest.rb
+++ abramov_minitest.rb
@@ -1,11 +1,12 @@
 require 'minitest/autorun'
 require './abramov'
-include Abramov
 
+# MiniTest
 class TestAbramov < Minitest::Test
-	def test_task_178b
-		assert_equal(3, task_178b(1, 10))
-	end
+  include Abramov
+  def test_task_178b
+    assert_equal(3, task_178b(1, 10))
+  end
 
   def test_task_178v
     assert_equal(3, task_178v(1, 36))
