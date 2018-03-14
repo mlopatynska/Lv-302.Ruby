1de1d9a20835f89f2cdbe66a4e6ff8c09cb4cc88
gnaizer <gntnaizer@gmail.com>
2018-03-14 17:30:06 +0200

    last changes

--- abramov.rb
+++ abramov.rb
@@ -5,20 +5,18 @@ module Abramov
     num.to_s.split(//).inject(0) { |sum, x| sum + x.to_i }
   end
 
-# 324. Даны целые числа p и q. Получить все делители числа q, взаимно простые с p.
+  # 324. Даны целые числа p и q. Получить все делители числа q, взаимно простые с p.
   def deviders(num)
-    (2..num).inject([]) { |dev, x| dev << x if (num % x).zero? dev }
+    (2..num).inject([]) { |dev, x| (num % x).zero? ? dev << x : dev }
   end
 
   def task_324(sum, num)
-    q_deviders = deviders(num)
-    p_deviders = deviders(sum)
-    q_deviders
-      .inject([]) { |dev, n| (p_deviders & deviders(n)) == [] ? dev << n : dev }
+    deviders(num).inject([]) { |dev, n| (deviders(sum) & deviders(n)) == [] ? dev << n : dev }
   end
 
-# 557. Дано натуральное число n (n ≥ 2). Найти все меньшие n простые числа, используя решето Эратосфена.
+  # 557. Дано натуральное число n (n ≥ 2). Найти все меньшие n простые числа, используя решето Эратосфена.
   def task_557(n)
     (2..n).inject([]){ |result, number| ((2...number).map{ |x| number % x }).include?(0) ? result : result << number }
   end
 end
+
