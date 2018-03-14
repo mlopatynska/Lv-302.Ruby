07d58090ffbbc7f98339bd731bf93193b5c588e7
Petro Smachylo <retrosmachylo@gmail.com>
2018-03-14 12:27:50 +0200

    Update abramov_test.rb

--- abramov_test.rb
+++ abramov_test.rb
@@ -3,16 +3,13 @@ require './abramov.rb'
 
 class TestAbramovTasks < Minitest::Unit::TestCase
 include Abramov
-	def test_task_88v
-		#assert_equal(28, task_88v(82))
-		assert_raises(ArgumentError) do
-			task_88v(-58)
-		end
-	end
-	def test_task_329
-		assert_equal([6, 15, 24, 33, 42, 51, 60, 105, 114], task_329(123, 36))
-	end
-	def test_task_562
-		assert_equal(false, task_562(555))
-	end
+def test_task_88v
+  assert_equal(28, task_88v(82))
+end
+def test_task_329
+  assert_equal([6, 15, 24, 33, 42, 51, 60, 105, 114], task_329(123, 36))
+end
+def test_task_562
+  assert_equal(false, task_562(555))
+end
 end
