68c964c20ebf32fcb9043d4fa3353982f3215b2f
VitaliiChverenko <vitchv96@gmail.com>
2018-03-14 10:22:07 +0200

    Added comments

--- abramov.rb
+++ abramov.rb
@@ -1,19 +1,24 @@
 # Functions for abramov tasks
 module Abramov
+# 224. Дано натуральне число n. Дістати всі натуральні дільники.
   def natural?(n)
     n.is_a?(Integer) && n > 0
   end
 
-  def simple?(n)
-    (2...n).all? { |num| n % num != 0 }
-  end
-
-  def divider(n)
+    def divider(n)
     (1..n).inject([]) do |i, x|
       (n % x).zero? ? i << x : i
     end
   end
 
+# 325. Дано натуральне число n. Дістати всі прості дільники.
+  def simple?(n)
+    (2...n).all? { |num| n % num != 0 }
+  end
+
+# 561. Дано натуральне число n. Серед чисел 1, ..., n найти всі
+# такі, запис котрих співпадає з останніми цифрами запису їх
+# квадрата (наприклад, 6 2 = 36, 25 2 = 625 и т. д.).
   def square?(n)
     (n**2).to_s.end_with?(n.to_s)
   end
---
+++ ruby
@@ -0,0 +1,1 @@
+/usr/share/rvm/rubies/ruby-2.4.1/bin/ruby
